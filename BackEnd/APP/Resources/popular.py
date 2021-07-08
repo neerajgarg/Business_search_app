6ddd793a90a5d4c6968325dc37ab0a036410064d
neeraj <neerajgarg400@gmail.com>
2021-07-08 22:04:41 +0530

    update for popular.py

--- BackEnd/APP/Resources/popular.py
+++ BackEnd/APP/Resources/popular.py
@@ -1,5 +1,5 @@
 from flask_restful import reqparse, Resource
-
+import re
 # User-defined Modules
 from APP import Mongodb
 from APP.Resources import projection
@@ -20,18 +20,55 @@ class Popular(Resource):
         parser = reqparse.RequestParser(bundle_errors=True)
         parser.add_argument(name='search_type', location='args', type=str, required=True)
         parser.add_argument(name='keyword', location='args', type=str, required=True)
+        parser.add_argument(name='Country', location='args', type=str, required=True)
+        parser.add_argument(name='Industry', location='args', type=str, required=True)
+        parser.add_argument(name='JobTitle', location='args', type=str, required=True)
         args = parser.parse_args(strict=True)
         try:
             path = ['JobTitle', 'AssetName', 'CampaignName', 'CompanyName', 'Industry']
             if args.get('search_type') == 'job':
                 path = 'JobTitle'
+            path1 = ['JobTitle']
             query = {
                 'index': 'Text_Search_Index',
                 'text': {'path': path, 'query': args.get('keyword')}
             }
+
+
+
+
+            query2 = {
+
+                "Industry": {"$eq": args.get('Industry')}
+            }
+            query3 = {
+
+                "Country":{"$eq": args.get('Country')}
+            }
+            # query4 = {
+            #
+            #     "$search":  args.get('JobTital')
+            # }
+
+            query4 = {
+                'index': 'Text_Search_Index',
+                'text': {'path': path1, 'query': args.get('JobTitle')}
+            }
+
+
+
+            pattern = r'[^A-Za-z ]'
+            regex = re.compile(pattern)
+            result1 = regex.sub('', args.get('JobTitle')).split(' ')
+
+
+
             addon_score = self._scorecalculator(filters=query, score=args.get('score', 100))
+
+
+
             pipeline = [
-                {'$search': query},
+                {'$search': query} ,{'$match': query2},{'$match': query3},
                 {'$project': projection},
                 {'$skip': 0},
                 {'$limit': args.get('limit', 20)}
@@ -42,7 +79,11 @@ class Popular(Resource):
             output = list()
             for i in response:
                 i['score'] = int(i['score'] + addon_score)
-                output.append(i)
+                print(i['job_title'])
+                print(args.get('JobTitle'))
+                if any(ele in i['job_title'] for ele in result1):
+                    print(any(ele in i['job_title'] for ele in result1))
+                    output.append(i)
             result = dict()
             result['status'] = 'sucess'
             result['data'] = output
